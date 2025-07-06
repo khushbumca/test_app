45a90021ca46a4d920976e7f323c584b89e77098
Khushbu Maheshwari <khushbumca@gmail.com>
2025-07-06 18:53:40 +0530

    Green - test case pass - adding 2 numbers from comma-separated string - check for equal and not equal case

--- app/services/calculator_service.rb
+++ app/services/calculator_service.rb
@@ -1,6 +1,8 @@
 class CalculatorService
   
   def self.call(str)
-    return 0 if str.strip.empty?
+    return 0 if str.strip.empty?  
+    str = str.split(",").map(&:to_i)
+    return str.sum
   end
 end
