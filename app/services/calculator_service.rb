cf5edc6f283f57895612680df91c83501a7473e6
Khushbu Maheshwari <khushbumca@gmail.com>
2025-07-06 20:15:58 +0530

    Grren - negative number test case pass - return throw an exception with negative numbers comma separted

--- app/services/calculator_service.rb
+++ app/services/calculator_service.rb
@@ -14,7 +14,17 @@ class CalculatorService
         # separated by , and convert to integer number  and then use inject method to array for adding numbers
         new_string = new_string.gsub(/\n/, ',').split(",").map(&:to_i)
     end
+    pos, neg = new_string.partition{|x| x.positive?}
+    p neg
+    neg = neg.select{|x| x != 0}
+    p neg
+    unless neg.empty?
+      raise "negative numbers not allowed #{neg.join(',')}" 
+    else 
+      return new_string.inject(&:+)
+    end
     
-    return new_string.inject(&:+)
+    
+    
   end
 end
