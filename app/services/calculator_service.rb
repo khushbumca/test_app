c8fc4e2b7636f52df564b1a38ed7c12de17e8b53
Khushbu Maheshwari <khushbumca@gmail.com>
2025-07-06 19:15:52 +0530

    Green - test case pass - for handle new lines between numbers (instead of commas)

--- app/services/calculator_service.rb
+++ app/services/calculator_service.rb
@@ -3,6 +3,6 @@ class CalculatorService
   def self.call(str)
     return 0 if str.strip.empty?   
     # separated by , and convert to integer number  and then use inject method to array for adding numbers
-    return str.split(",").map(&:to_i).inject(&:+)
+    return str.gsub(/\n/, ',').split(",").map(&:to_i).inject(&:+)
   end
 end
